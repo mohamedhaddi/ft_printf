2c3153c1957aa062e0c408a42f0b7379703d3a29
Mohamed Haddi <mhaddi@e1r4p8.1337.ma>
2020-02-19 20:57:07 +0100

    checks if the precision dot was found before setting zeroflag_found to true, to not confuse a 0 after the precision with the 0 flag

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -167,7 +167,7 @@ int	ft_printf(const char *s, ...)
 				minusflag_found = (*str == '-' && !precisiondot_found) ? 1 : minusflag_found;
 				// plus_found = (*str == '+') ? 1 : plus_found;
 				// %
-				zeroflag_found = (*str == '0') ? 1 : zeroflag_found;
+				zeroflag_found = (*str == '0' && !precisiondot_found) ? 1 : zeroflag_found;
 				precisiondot_found = (*str == '.') ? 1 : precisiondot_found;
 				precision_val = (*str == '.' && !ft_isdigit(*(str + 1))) ? 0 : precision_val; // if no precision was set after the dot, it's 0 by default
 				if (*str != '0' && (ft_isdigit(*str) || *str == '*')) // we don't want 0 flag to be counted as one of these numbers
